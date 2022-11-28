5d4629df54cfbe8303c1762440a42b5b7306aa11
Sinkó Ábel <sinkoabel@gmail.com>
2022-11-28 22:13:06 +0100

    Created the basics of the screen changes and folder structures for both screens and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import HomeScreen from "./Screens/HomeScreen";
 
 function App() {
+
+   const [activeScreen, setActiveScreen]: any = useState("homeScreen");
+
+   const screens: any = {
+     homeScreen: <HomeScreen/>,
+   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#333333] h-[100vh]">
+      <div>
+        {screens[activeScreen]}
+      </div>
     </div>
   );
 }
---
+++ src/Screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = (props: any) => {
+    return (
+        <div>
+            <h2 className="font-bold text-2xl text-white">Home screen</h2>
+        </div>
+    );
+};
+
+export default HomeScreen;
