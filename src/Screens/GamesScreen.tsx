e0db20e6b39be40f5844bd8642a25408f9efb7e7
Sinkó Ábel <sinkoabel@gmail.com>
2022-12-03 15:39:56 +0100

    Added more blank screens, will be used in the future development.

---
+++ src/Screens/GamesScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GamesScreen = (props: any) => {
+    return (
+        <div>
+            Games selector Screen
+        </div>
+    );
+};
+
+export default GamesScreen;
