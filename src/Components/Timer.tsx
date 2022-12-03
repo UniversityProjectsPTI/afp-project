e582f4d4deb07cb146a2c8fae1fe25b53292d036
Sinkó Ábel <sinkoabel@gmail.com>
2022-12-03 15:10:28 +0100

    Finished up the timer component

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -5,6 +5,12 @@ const Timer = (props: any) => {
 
     const [isCountedDown, setIsCountedDown] = useState(false);
     const [remainingTime, setRemainingTime] = useState(props.time);
+    const [percentageNum, setPercentageNum] = useState(100);
+
+    const timerDecreaseWidth = () => {
+        const decreaseRatio = 100 / props.time;
+        setPercentageNum(percentageNum-decreaseRatio);
+    }
 
     useEffect(() => {
         if (remainingTime === 0) {
@@ -13,6 +19,7 @@ const Timer = (props: any) => {
         } else {
             const intervalID = setInterval(() => {
                 setRemainingTime(remainingTime - 1);
+                timerDecreaseWidth();
             }, 1000);
             return () => clearInterval(intervalID);
         }
@@ -24,8 +31,15 @@ const Timer = (props: any) => {
     }
 
     return (
-        <div>
-            <p className="text-2xl text-white">{remainingTime} mp</p>
+        <div className="flex flex-row justify-center items-center space-x-5">
+            <div className="rounded-full">
+                <div className="w-[300px] bg-gray-400 h-2 rounded-full">
+                    <div className="bg-[#ffd428] h-2 rounded-full transform" style={{width: percentageNum+'%'}}></div>
+                </div>
+            </div>
+            <div className="w-20 mb-1">
+                <div className="text-xl font-bold text-white">{remainingTime} mp</div>
+            </div>
         </div>
     );
 };
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import Timer from "../Components/Timer";
 const HomeScreen = (props: any) => {
     return (
         <div className="flex justify-center h-screen items-center flex-col">
-            <h2 className="font-bold text-4xl text-yellow-500">Üdvözöllek</h2>
+            <h2 className="font-bold text-4xl text-[#ffd428] uppercase">Üdvözöllek!</h2>
         </div>
     );
 };
