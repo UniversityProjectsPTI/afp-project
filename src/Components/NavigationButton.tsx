0d903c3c5401982a4493ae6dee750a1d2ae44061
Tarhos László <tlaszlo96@gmail.com>
2022-12-05 00:34:42 +0100

    Created 2 different button type component for navigation and for answer

---
+++ src/Components/NavigationButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavigationButton = (props: any) => {
+    return (
+        <button type="button" className="rounded-md border-2 border-[#ffd428] mt-5 flex items-center justify-center ml-8 hover:text-white text-xl text-[#ffd428] font-medium text-center p-2">
+            {props.children}
+        </button>
+    );
+};
+
+export default NavigationButton;
