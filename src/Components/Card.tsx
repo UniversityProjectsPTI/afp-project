a289e132c0765b161eb7c640f665a36f2367f2ec
tanyiG <tanyi.gyozo@gmail.com>
2022-12-06 18:29:15 +0100

    Card component corrected version

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const NavigationButton = (props: any) => {
+const Card = (props: any) => {
     return (
-        <div class="rounded-lg border-2 border-[#ffd428] mt-5 flex items-center justify-center ml-8 hover:text-white text-base text-[#ffd428] font-medium text-center p-2">
+        <div className="shadow-lg w-[650px] h-[650px] rounded-lg bg-[#666666] mt-5 flex items-center
+                        justify-center ml-8 text-base text-[#ffd428] font-medium text-center p-2">
             {props.children}
         </div>
     );
 };
 
-export default NavigationButton;
+export default Card;
