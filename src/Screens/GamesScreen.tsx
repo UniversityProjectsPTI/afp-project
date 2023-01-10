63fd47dab583e36deee482ac89ca98dbf76699d7
Tarhos László <tlaszlo96@gmail.com>
2023-01-10 01:34:50 +0100

    The quiz game Start button added and done.

--- src/Screens/GamesScreen.tsx
+++ src/Screens/GamesScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Components/Card';
+import NavigationButton from '../Components/NavigationButton';
 
 const GamesScreen = (props: any) => {
     return (
@@ -8,6 +9,9 @@ const GamesScreen = (props: any) => {
                 <div className="mb-20">
                     <h2 className="font-bold text-3xl text-[#ffd428] uppercase my-15">Kvízjáték</h2>
                     <h3 className="text-2xl p-2 mt-10">Teszteld a tudásod egy kvíz játékkal.</h3>
+                    <div className="flex items-center justify-center text-center mt-6">
+                        <NavigationButton setActiveScreen={props.setActiveScreen} nextScreen={'quizGame'}>A kvízjáték indítása</NavigationButton>
+                    </div>
                 </div>
             </Card>
         </div>
