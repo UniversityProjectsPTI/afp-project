a14e6ab75488af309ae0b7b40dda9055346a1627
Tarhos László <tlaszlo96@gmail.com>
2023-01-10 12:40:54 +0100

    Created first playable demo

--- src/Screens/QuizScreen.tsx
+++ src/Screens/QuizScreen.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Card from '../Components/Card';
+import QuizCounterHead from '../Components/QuizCounterHead';
+import questions from '../config/questions';
+import NavigationButton from '../Components/NavigationButton';
 
 const QuizScreen = (props: any) => {
+
+    const [quizCounter, setQuizCounter] = useState(1);
+    const quizQuestions = useState(3);
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [isGameEnd, setIsGameEnd] = useState(false);
+
+    const gameQuestions = {...questions};
+    const gameAnswer1 = gameQuestions[currentQuestion].answer1;
+    const gameAnswer2 = gameQuestions[currentQuestion].answer2;
+
+    const handleAnswerClick = (ans: string) => {
+        if (ans === gameQuestions[currentQuestion].correctAnswer)
+            setCorrectAnswers(correctAnswers+1);
+
+        if (quizCounter !== 3){
+            setQuizCounter(quizCounter+1);
+            setCurrentQuestion(currentQuestion+1);
+        } else {
+            setIsGameEnd(true);
+        }
+    };
+
+
+
     return (
-        <div>
-            QuizScreen
+        <div className="flex items-center justify-center text-center p-2">
+            <Card className='flex items-center justify-center text-center p-2'>
+
+                {!isGameEnd ?
+                    <div>
+                        <QuizCounterHead currentQuestion={quizCounter} questionNumbers={quizQuestions}/><br/>
+                        
+                        <div>
+                            {gameQuestions[currentQuestion].question}
+                        </div>
+
+                        <div className='mt-5 bold' onClick={() => handleAnswerClick(gameAnswer1)}>
+                            {gameQuestions[currentQuestion].answer1}
+                        </div>
+                        <div className='mt-5 bold' onClick={() => handleAnswerClick(gameAnswer2)}>
+                            {gameQuestions[currentQuestion].answer2}
+                        </div>
+                    </div>
+                    : 
+                    <div>
+                        <div>
+                            Játék vége
+                        </div>
+                        <div>
+                            Elért pontok: {correctAnswers}
+                        </div>
+                        <div className='mt-5'>
+                            Visszatérés a játékválasztó képernyőre.
+                        </div>
+                        <div className='flex justify-center'>
+                            <NavigationButton setActiveScreen={props.setActiveScreen} nextScreen={'games'}>Vissza</NavigationButton>
+                        </div>
+                    </div>
+                }
+
+            </Card>
         </div>
     );
 };
--- src/config/questions.ts
+++ src/config/questions.ts
@@ -1,1 +1,32 @@
-export {}
+export interface IQuestion {
+    id: number;
+    question: string;
+    correctAnswer: string;
+    answer1: string;
+    answer2: string;
+}
+
+
+const questions: IQuestion[] = [
+    {
+        id: 1,
+        question: 'A reukrzió minden esetben lassabb mint az iteratív megvalósítás.',
+        correctAnswer: 'Igaz',
+        answer1: 'Igaz',
+        answer2: 'Hamis'
+    },{
+        id: 2,
+        question: 'Melyik mértéket használjuk az áramerősség mérésére?',
+        correctAnswer: 'Amper',
+        answer1: 'Amper',
+        answer2: 'Volt'
+    },{
+        id: 3,
+        question: 'Melyik a 6. bolygó a naptól?',
+        correctAnswer: 'Szaturnusz',
+        answer1: 'Uránsz',
+        answer2: 'Szaturnusz',
+    } 
+];
+
+export default questions;
