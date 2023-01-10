725c952290521ba1bbf9312e6177cd8c94708e22
Tarhos László <tlaszlo96@gmail.com>
2023-01-10 01:18:34 +0100

    In Game Screen Card the quiz game texts added

--- src/Screens/GamesScreen.tsx
+++ src/Screens/GamesScreen.tsx
@@ -5,7 +5,10 @@ const GamesScreen = (props: any) => {
     return (
         <div className="flex items-center justify-center text-center p-2">
             <Card className="flex items-center justify-center text-center p-2">
-         
+                <div className="mb-20">
+                    <h2 className="font-bold text-3xl text-[#ffd428] uppercase my-15">Kvízjáték</h2>
+                    <h3 className="text-2xl p-2 mt-10">Teszteld a tudásod egy kvíz játékkal.</h3>
+                </div>
             </Card>
         </div>
     );
