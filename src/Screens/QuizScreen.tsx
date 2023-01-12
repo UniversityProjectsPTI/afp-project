ffd801a86d8f2b8694c441dc0bcb3dee37f3e357
Tarhos László <tlaszlo96@gmail.com>
2023-01-12 08:40:30 +0100

    Card component added to QuizScreen.

--- src/Screens/QuizScreen.tsx
+++ src/Screens/QuizScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Card from '../Components/Card';
 
 const QuizScreen = (props: any) => {
     return (
-        <div>
-            QuizScreen
-        </div>
+        <div className="flex items-center justify-center text-center p-2">
+            <Card className='flex items-center justify-center text-center p-2'>          
+
+            </Card>
+    </div>
     );
 };
 
