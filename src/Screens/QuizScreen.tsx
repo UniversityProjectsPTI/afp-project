30dd8c79e688b12bb1a7a3f604df1e4489731023
Tarhos László <tlaszlo96@gmail.com>
2023-01-14 19:13:11 +0100

    Variables added and displaying the questions added

--- src/Screens/QuizScreen.tsx
+++ src/Screens/QuizScreen.tsx
@@ -5,11 +5,26 @@ import NavigationButton from '../Components/NavigationButton';
 import questions from '../config/questions';
 
 const QuizScreen = (props: any) => {
-    return (
-        <div className="flex items-center justify-center text-center p-2">
-            <Card className='flex items-center justify-center text-center p-2'>          
 
-            </Card>
+    const [quizCounter, setQuizCounter] = useState(1);
+    const quizQuestions = useState(3);
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [isGameEnd, setIsGameEnd] = useState(false);
+
+    const gameQuestions = {...questions};
+
+    return (
+        <div className="flex justify-center p-2">
+        <Card className="flex flex-row">
+
+            <div className="flex-row">
+                {gameQuestions[currentQuestion].cardQuestion}
+            </div>
+
+            
+
+        </Card>
     </div>
     );
 };
