53829e24f5ba2bc05317927d62d3069a1c00fe7b
Tarhos László <tlaszlo96@gmail.com>
2023-01-14 18:59:20 +0100

    Fixed/Modified Card component items display to rows

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = (props: any) => {
     return (
-        <div className="shadow-lg w-[650px] h-[650px] rounded-lg bg-[#666666] mt-5 flex items-center
+        <div className="shadow-lg w-[650px] h-[650px] rounded-lg bg-[#666666] mt-5 items-center
                         justify-center ml-8 text-base text-[#ffd428] font-medium text-center p-2">
             {props.children}
         </div>
