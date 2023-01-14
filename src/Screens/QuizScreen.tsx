62a40cba7f78d1c1ab3bc1716c581182637e0e4d
Tarhos László <tlaszlo96@gmail.com>
2023-01-14 19:25:19 +0100

    Answer click handle function added

--- src/Screens/QuizScreen.tsx
+++ src/Screens/QuizScreen.tsx
@@ -14,17 +14,29 @@ const QuizScreen = (props: any) => {
 
     const gameQuestions = {...questions};
 
+    const handleAnswerClick = (ansid: number) => {
+        if (ansid === gameQuestions[currentQuestion].correctAnswer)
+            setCorrectAnswers(correctAnswers+1);
+
+        if (quizCounter !== 3){
+            setQuizCounter(quizCounter+1);
+            setCurrentQuestion(currentQuestion+1);
+        } else {
+            setIsGameEnd(true);
+        }
+    };
+
     return (
         <div className="flex justify-center p-2">
         <Card className="flex flex-row">
-
+            
             <div className="flex-row">
                 {gameQuestions[currentQuestion].cardQuestion}
             </div>
 
             {gameQuestions[currentQuestion].cardAnswers.map((answerItem) => {
                     return (
-                        <div className="flex-row cursor-pointer" key={answerItem.id}>
+                        <div className="flex-row cursor-pointer" onClick={() => handleAnswerClick(answerItem.id)} key={answerItem.id}>
                             {answerItem.answer}
                         </div>
                     );
