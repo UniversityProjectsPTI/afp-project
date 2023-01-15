8535cd2fe0439a0b8dc356f99693923120fdf50d
Sinkó Ábel <sinkoabel@gmail.com>
2023-01-15 20:58:50 +0100

    fixing GamesScreen.tsx's css styling

--- src/Screens/GamesScreen.tsx
+++ src/Screens/GamesScreen.tsx
@@ -5,8 +5,8 @@ import NavigationButton from '../Components/NavigationButton';
 const GamesScreen = (props: any) => {
     return (
         <div className="flex items-center justify-center text-center p-2">
-            <Card className="flex items-center justify-center text-center p-2">
-                <div className="mb-20">
+            <Card additionClass="mt-5">
+                <div className="mt-20">
                     <h2 className="font-bold text-3xl text-[#ffd428] uppercase my-15">Kvízjáték</h2>
                     <h3 className="text-2xl p-2 mt-10">Teszteld a tudásod egy kvíz játékkal.</h3>
                     <div className="flex items-center justify-center text-center mt-6">
